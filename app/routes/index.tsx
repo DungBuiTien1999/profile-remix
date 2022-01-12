cec16ec561102881839dfcaafb37a4f735659f62
DungBuiTien1999 <nguyentonhi199@gmail.com>
2022-01-12 16:58:51 +0700

    setup project

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }} className="w-screen h-screen bg-slate-500 flex justify-center items-center">
+      <h1 className=" text-8xl text-white font-bold">Welcome to Remix</h1>
+    </div>
+  );
+}
